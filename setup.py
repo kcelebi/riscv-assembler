7eec44ae24339a3872bb2a8b77425fa6c433d58f
Kaya Celebi <kayacelebi17@gmail.com>
2020-12-23 04:26:21 -0500

    minor changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="riscv_assembler",
-    version="0.0.6",
+    version="0.0.7",
     author="Kaya Çelebi",
     author_email="kayacelebi17@gmail.com",
     description="RISC-V assembler",
