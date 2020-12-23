8d46e72d3f0677292210f9ee173a2b783ed812e6
Kaya Celebi <kayacelebi17@gmail.com>
2020-12-23 00:05:02 -0500

    removed test dir

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,4 +1,4 @@
-from riscinterpreter.convert_class import AssemblyConverter
+from convert_class import AssemblyConverter
 #fix above ^^^
 #TESTS
 
@@ -7,14 +7,5 @@ def test0():
 	cnv = AssemblyConverter("tests/assembly/simple.s", "tb")
 
 	cnv.convert()
-
-	#need to make expected outputs
-	'''ans = "tests/expected/simple/txt/simple.txt"
-	a = open(ans,"r")
-	a_line = a.readline()
-
-	file = "output/simple/txt/simple.txt"
-	f = open(file,"r")
-	f_line = f.readline()'''
 
 test0()
