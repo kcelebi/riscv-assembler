c515ed990d502a64674b6c6315dee121301b47f1
Kaya Celebi <kayacelebi17@gmail.com>
2020-12-23 04:20:55 -0500

    directory changes, README change

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# riscv-interpreter
-RISC-V Assembly code interpreter package
+# riscv-assembler
+RISC-V Assembly code assembler package.
 
-test
+This package contains functions that act as assemblers for RISC-V assembly code. 
--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="risc_translate",
-    version="0.0.1",
+    name="riscv_assembler",
+    version="0.0.6",
     author="Kaya Çelebi",
     author_email="kayacelebi17@gmail.com",
-    description="RISC-V translator",
+    description="RISC-V assembler",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kcelebi/riscv-translator",
+    url="https://github.com/kcelebi/riscv-assembler",
     packages=setuptools.find_packages(),
-    package_dir={'risc_translate':'.'},
-    package_data={'risc_translate':['data/*.dat']},
+    package_dir={'riscv_assembler':'.'},
+    package_data={'riscv_assembler':['data/*.dat']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
