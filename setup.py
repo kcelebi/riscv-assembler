d05f1030a3ba9216809b128470e8cfaf02ce35e8
Kaya Celebi <kayacelebi17@gmail.com>
2020-12-23 04:08:39 -0500

    updated directory names

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="riscinterpreter",
+    name="risc-translate",
     version="0.0.5",
     author="Kaya Çelebi",
     author_email="kayacelebi17@gmail.com",
-    description="RISC-V interpreter",
+    description="RISC-V translator",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kcelebi/riscv-interpreter",
+    url="https://github.com/kcelebi/riscv-translator",
     packages=setuptools.find_packages(),
-    package_dir={'riscinterpreter':'.'},
-    package_data={'riscinterpreter':['data/*.dat']},
+    package_dir={'risc-translate':'.'},
+    package_data={'risc-translate':['data/*.dat']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
