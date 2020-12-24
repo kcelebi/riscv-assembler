82153b8f7fdcdc197b9397aeabff0a951434f2a3
Kaya Celebi <kayacelebi17@gmail.com>
2020-12-24 17:17:19 -0500

    Prep for 1.0.10, req fix and .travis included

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,10 +1,22 @@
-from convert_class import AssemblyConverter
+'''
+TASKS
+
+[] fix __all__
+[] make getOutputType tests and other functions
+[] get rid of 2.7 suport
+[] push to add requirements
+[] addPseudo function
+'''
+
+from riscv_assembler.convert import AssemblyConverter
 #TESTS
 
 #test simple.s file, writes to txt and bin
+def func0():
+	cnv = AssemblyConverter()
+
+	return cnv.convert_ret()
+
 def test0():
-	cnv = AssemblyConverter("tests/assembly/simple.s", "tb")
-
-	cnv.convert()
-
+	assert func0() == []
 test0()
