d034433ae3558caa97af7c48888c3e113884bf32
Kaya Celebi <kayacelebi17@gmail.com>
2020-12-27 21:27:14 -0500

    moved funcs to utils, fixed global declarations, added tests

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="riscv-assembler",
-    version="1.0.10",
+    version="0.1.13",
     author="Kaya Çelebi",
     author_email="kayacelebi17@gmail.com",
     description="RISC-V assembler",
---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+from riscv_assembler.utils import *
+from pathlib import Path
+import pytest
+
+def func0():
+	return nibbleForm("000011110000010100001111")
+
+def test_0():
+	assert func0() == "0000\t1111\t0000\t0101\t0000\t1111"
