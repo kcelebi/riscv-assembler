7c5724d193150ab29fa9aacd202c572e70136fbc
Kaya Celebi <kayacelebi17@gmail.com>
2020-12-30 16:14:51 -0500

    1.0.11 now available

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pytest
 
 def func0():
-	return nibbleForm("000011110000010100001111")
+	return nibbleForm("000011110000010100001111",'\t')
 
 def test_0():
 	assert func0() == "0000\t1111\t0000\t0101\t0000\t1111"
