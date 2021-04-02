e65786f7502e3d7b09010a2fb430b8c315ee9d6f
Kaya Celebi <kayacelebi17@gmail.com>
2021-04-02 06:38:51 -0400

    updated impl real quick

--- riscv_assembler/project_convert.py
+++ riscv_assembler/project_convert.py
@@ -10,18 +10,12 @@ class NoAssemblyDirectory( Exception ):
 
 class ProjectConverter:
 
-	def __init__(self, root = '', output_type='b', nibble = False, filename = "", hexMode = False):
+	def __init__(self, root = '', output_type='b', nibble = False, hexMode = False):
 		self.root = root #should be relative path from python script using pkg
 		if root == '':
 			self.root = os.getcwd()
 
-		self.hexMode = hexMode
-		self.nibble = nibble
-
-		if "b" not in output_type and "t" not in output_type and "p" not in output_type:
-			raise IncorrectOutputType()
-		else:
-			self.output_type = output_type
+		self.converter = AssemblyConverter(output_type=output_type, nibble=nibble, hexMode=hexMode)
 
 		self.files = [x for x in os.listdir(self.root) if x[-2:] == '.s'] #need to raise error just in case
 		if len(self.files) == 0:
@@ -30,11 +24,10 @@ class ProjectConverter:
 
 	def __str__(self):
 		return "**\n  	ProjectConverter(output_type={}, nibble={}, hexmode={})\n\t- root: {}\n\t- Files: {}\n**".format(
-			self.output_type, self.nibble,
-			self.hexMode,self.root,
+			self.converter.output_type, self.converter.nibble,
+			self.converter.hexMode, self.root,
 			self.files
 		)
 
 	#convert the whole project to machine
 	def convert(self):
-		ac = AssemblyConverter()
