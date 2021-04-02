a386d977dd19f6d76384d874137a3141e2e1c263
Kaya Celebi <kayacelebi17@gmail.com>
2021-04-02 06:38:51 -0400

    updated impl real quick

--- riscv_assembler/test_project.py
+++ riscv_assembler/test_project.py
@@ -3,11 +3,6 @@ from convert import *
 
 pc = ProjectConverter('tests/assembly')
 
-ac = AssemblyConverter(output_type='r')
-
-lines = ac.convert('tests/assembly/test2.s')
-
-print(lines)
 #TEST CASES
 # 1. w/ or w/o filepath
 # 2. incorrect filepath raises error
