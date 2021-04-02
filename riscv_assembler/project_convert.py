5678f26d150cf68d09fbe3e6e79a77f9bc10cc94
Kaya Celebi <kayacelebi17@gmail.com>
2021-04-02 06:19:34 -0400

    adding basic framework to project class

--- riscv_assembler/project_convert.py
+++ riscv_assembler/project_convert.py
@@ -1,13 +1,33 @@
 from convert import AssemblyConverter
 import os
 
+__all__ = ['ProjectConverter']
+
 class ProjectConverter:
 
-	def __init__(root):
-		self.root = root
-		self.files = []
+	def __init__(self, root = '', output_type='b', nibble = False, filename = "", hexMode = False):
+		self.root = root #should be relative path from python script using pkg
+		if root == '':
+			self.root = os.getcwd()
 
+		self.hexMode = hexMode
+		self.nibble = nibble
 
-	def __str__():
-		return "ProjectConverter rooted at {}".format(self.root)
+		if "b" not in output_type and "t" not in output_type and "p" not in output_type:
+			raise IncorrectOutputType()
+		else:
+			self.output_type = output_type
 
+		self.files = [x for x in os.listdir(self.root) if x[-2:] == '.s'] #need to raise error just in case
+		#take only .s files
+
+	def __str__(self):
+		return "**\n  	ProjectConverter(output_type={}, nibble={}, hexmode={})\n\t- root: {}\n\t- Files: {}\n**".format(
+			self.output_type, self.nibble,
+			self.hexMode,self.root,
+			self.files
+		)
+
+	#convert the whole project to machine
+	def convert(self):
+		ac = AssemblyConverter()
---
+++ riscv_assembler/test_project.py
@@ -0,0 +1,6 @@
+from project_convert import *
+from convert import *
+
+pc = ProjectConverter('tests/assembly')
+
+print(str(pc))
