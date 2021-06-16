29b18da333a0a75559d54891950b8c78423e90da
Kaya Celebi <kayacelebi17@gmail.com>
2021-06-16 15:54:06 -0400

    updated tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,5 +5,12 @@ import pytest
 def func0():
 	return nibbleForm("000011110000010100001111",'\t')
 
+def func1():
+	tk = Toolkit()
+	return tk.R_type('add', 'x0','x0','x0')
+
 def test_0():
 	assert func0() == "0000\t1111\t0000\t0101\t0000\t1111"
+
+def test_1():
+	assert func1() == '00000000000000000000000000110011'
